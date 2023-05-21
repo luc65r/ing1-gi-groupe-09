1823026f313eee2b636cf96808ed19221dd05ab2
Lucas Ransan <lucas@ransan.tk>
2023-05-21 14:42:46 +0200

    traduction

--- resources/lang/fr/passwords.php
+++ resources/lang/fr/passwords.php
@@ -2,8 +2,8 @@
 
 return [
 
-    'reset' => 'Votre mot de passe a été réinitialisé&nbsp!',
-    'sent' => 'Nous vous avons envoyé le lien de réinitialisation de votre mot de passe par courriel&nbsp!',
+    'reset' => 'Votre mot de passe a été réinitialisé !',
+    'sent' => 'Nous vous avons envoyé le lien de réinitialisation de votre mot de passe par courriel !',
     'throttled' => 'Veuillez attendre avant de réessayer.',
     'token' => 'Le jeton de réinitialisation du mot de passe est invalide.',
     'user' => 'Nous ne trouvons pas d’utilisateur avec cette adresse courriel.',
