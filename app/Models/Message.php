83d286f59f2504b82b84d6d991f503ab26c2ae39
Lucas Ransan <lucas@ransan.tk>
2023-05-23 11:04:11 +0200

    add messages models and migrations

---
+++ app/Models/Message.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Message extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'subject',
+        'body',
+    ];
+
+    public function sender() {
+        return $this->belongsTo(User::class, 'sender');
+    }
+
+    public function recievers() {
+        return $this->belongsToMany(User::class);
+    }
+}
