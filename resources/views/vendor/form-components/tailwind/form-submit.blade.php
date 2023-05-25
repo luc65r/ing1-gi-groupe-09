82815f07e706b2881e1a0c3983158a3f034534f3
Lucas Ransan <lucas@ransan.tk>
2023-05-25 10:00:14 +0200

    make submit button visible

--- resources/views/vendor/form-components/tailwind/form-submit.blade.php
+++ resources/views/vendor/form-components/tailwind/form-submit.blade.php
@@ -1,8 +1,8 @@
 <div class="mt-6 flex items-center justify-between">
     <button {!! $attributes->merge([
-        'class' => 'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline',
-        'type' => 'submit'
-    ]) !!}>
+                'class' => 'inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150',
+                'type' => 'submit'
+                ]) !!}>
         {!! trim($slot) ?: __('Submit') !!}
     </button>
 </div>
