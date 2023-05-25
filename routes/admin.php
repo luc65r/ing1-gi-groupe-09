ad67ff3e40b831f0c5148d0977b56551bc21b919
Lucas Ransan <lucas@ransan.tk>
2023-05-25 10:00:00 +0200

    fix creating students

--- routes/admin.php
+++ routes/admin.php
@@ -4,9 +4,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
 Route::group(['middleware' => 'is:admin', 'prefix' => 'admin', 'as' => 'admin.'], function () {
-    // ...
-    Route::resource('/users', UserController::class);
+    Route::resource('/users', UserController::class)->except(['store']);
+    Route::get('/users/create/student', [UserController::class, 'createStudent'])->name('users.createStudent');
+    Route::get('/users/create/manager', [UserController::class, 'createManager'])->name('users.createManager');
+    Route::get('/users/create/admin', [UserController::class, 'createAdmin'])->name('users.createAdmin');
+    Route::post('/users/student', [UserController::class, 'storeStudent'])->name('users.storeStudent');
+    Route::post('/users/manager', [UserController::class, 'storeManager'])->name('users.storeManager');
+    Route::post('/users/admin', [UserController::class, 'storeAdmin'])->name('users.storeAdmin');
 });
-
-
-?>
