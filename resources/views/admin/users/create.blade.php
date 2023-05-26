f53127ebde7e1e4d0c15f1065683592195ef0bb8
Adrien Bernard <bernardadrien26@gmail.com>
2023-05-26 15:50:25 +0200

    ede

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -6,14 +6,26 @@
     </x-slot>
 
     <div id="choixType" class="py-12">
-        <div>
-            <a href="{{ route('admin.users.createStudent') }}">Etudiant</a>
+        <div class="mb-4">
+            <a href="{{ route('admin.users.createStudent') }}" class="custom-link bg-indigo-500">
+                <div class="flex items-center justify-center h-16 rounded-lg shadow-lg">
+                    <span class="text-white text-2xl font-bold" style="color:black;">Etudiant</span>
+                </div>
+            </a>
+        </div>
+        <div class="mb-4">
+            <a href="{{ route('admin.users.createManager') }}" class="custom-link bg-green-500">
+                <div class="flex items-center justify-center h-16 rounded-lg shadow-lg">
+                    <span class="text-white text-2xl font-bold" style="color:black;" >Gestionnaire</span>
+                </div>
+            </a>
         </div>
         <div>
-            <a href="{{ route('admin.users.createManager') }}">Gestionnaire</a>
-        </div>
-        <div>
-            <a href="{{ route('admin.users.createAdmin') }}">Administrateur</a>
+            <a href="{{ route('admin.users.createAdmin') }}" class="custom-link bg-red-500">
+                <div class="flex items-center justify-center h-16 rounded-lg shadow-lg">
+                    <span class="text-white text-2xl font-bold" style="color:black;">Administrateur</span>
+                </div>
+            </a>
         </div>
     </div>
 </x-app-layout>
