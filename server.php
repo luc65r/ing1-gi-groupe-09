37c3e50d6c0762b2e9e5a291a2a500b3ae732da3
Lucas Ransan <lucas@ransan.tk>
2023-05-30 14:46:05 +0200

    reverse proxy to stats

--- server.php
+++ server.php
@@ -18,4 +18,36 @@ if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
     return false;
 }
 
+if (str_starts_with($uri, '/stats/')) {
+    $session = curl_init("http://localhost:8001$uri");
+    $request_method = $_SERVER['REQUEST_METHOD'];
+    if ($request_method === 'POST') {
+        curl_setopt($session, CURLOPT_POST, true);
+        curl_setopt($session, CURLOPT_POSTFIELDS, file_get_contents("php://input"));
+    } else {
+        curl_setopt($session, CURLOPT_CUSTOMREQUEST, $request_method);
+    }
+    $request_headers = [];
+    foreach (getallheaders() as $key => $value)
+        $request_headers[] = "$key: $value";
+    curl_setopt($session, CURLOPT_HTTPHEADER, $request_headers);
+    curl_setopt($session, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($session, CURLOPT_HEADER, true);
+
+    $response = curl_exec($session);
+    $header_size = curl_getinfo($session, CURLINFO_HEADER_SIZE);
+    $response_body = substr($response, $header_size);
+    $response_httpcode = curl_getinfo($session, CURLINFO_HTTP_CODE);
+    $response_content_type = curl_getinfo($session, CURLINFO_CONTENT_TYPE);
+    $response_error = curl_error($session);
+    curl_close($session);
+
+    header("Content-type: $response_content_type", 1);
+    http_response_code($response_httpcode);
+    echo $response_error ? $response_error : $response_body;
+
+    return true;
+}
+
 require_once __DIR__.'/public/index.php';
