b7321925f55fe303190e5aed9a683e8a4525d55e
Lucas Loustalet <lucas.loustalet@orange.fr>
2023-05-31 15:16:37 +0200

    index project

--- resources/views/contests/projects/index.blade.php
+++ resources/views/contests/projects/index.blade.php
@@ -24,47 +24,61 @@
                             </tr>
                         </thead>
                         <tbody>
-                            @foreach ($projects as $project)
-                                <tr>
-                                    <div>
-                                        <td class="px-6 py-4 text-center">
-                                            <a>
-                                                {{ $project->name }}
+                            <div class="py-12">
+                                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                                    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                                        @is('admin')
+                                            <a href="{{ route('contests.projects.create', ['contest' => $contest]) }}">
+                                                Nouveau
                                             </a>
-                                        </td>
+                                        @endis
 
-                                        <td class="px-6 py-4 text-center ">
-
-                                            @is('admin')
-                                                <form action="{{ route('projects.destroy', $project->id) }}" method="POST">
-                                                    @csrf
-                                                    @method('DELETE')
-                                                    <button class="voirP rounded-lg"type="submit" class="btn btn-danger"
-                                                        onclick="return confirm('Êtes-vous sûr de vouloir supprimer cet utilisateur ?')">Supprimer</button>
-                                                </form>
+                                        <div class="p-6 bg-white border-b border-gray-200">
+                                            @foreach ($projects as $project)
                                                 <div>
-                                                    <a class="voirP rounded-lg"
-                                                        href="{{ route('projects.edit', $project->id) }}">Modifier</a>
-                                                </div>
-                                                <div>
-                                                    <button class="voirP rounded-lg" onclick="toggleSearchBar()">Assigner un
-                                                        gestionnaire au projet</button>
-                                                </div>
+                                                    <a href="{{ route('projects.show', $project->id) }}">
+                                                        {{ $project->name }}
+                                                    </a>
+                                                    @is('admin')
+                                                        <form action="{{ route('projects.destroy', $project->id) }}"
+                                                            method="POST">
+                                                            @csrf
+                                                            @method('DELETE')
+                                                            <button type="submit" class="btn btn-danger"
+                                                                onclick="return confirm('Êtes-vous sûr de vouloir supprimer cet utilisateur ?')">Supprimer</button>
+                                                        </form>
+                                                        <div>
+                                                            <a
+                                                                href="{{ route('projects.edit', $project->id) }}">Modifier</a>
+                                                        </div>
+                                                        <div>
+                                                            <form
+                                                                action="{{ route('projects.assign', ['project' => $project->id]) }}"
+                                                                method="POST">
+                                                                @csrf
+                                                                <div>
+                                                                    <label for="manager-choice">Sélectionner un manager
+                                                                        :</label>
+                                                                    <input list="list-manager" id="manager-choice"
+                                                                        name="manager-choice" autocomplete="off">
 
-                                                <div id="searchBar" style="display: none;">
-                                                    <input type="text" id="searchInput"
-                                                        placeholder="Rechercher un gestionnaire" oninput="searchManagers()">
-                                                    <ul id="searchResults"></ul>
+                                                                    <datalist id="list-manager">
+                                                                        @foreach ($managers as $manager)
+                                                                            <option value="{{ $manager->user->name }}">
+                                                                        @endforeach
+                                                                    </datalist>
+                                                                </div>
+                                                                <button type="submit">Assigner</button>
+                                                            </form>
+                                                        </div>
+                                                    @endis
                                                 </div>
-                                            @endis
+                                            @endforeach
 
-                                            <a href="{{ route('projects.show', $project->id) }}"
-                                                class="voirP rounded-lg">Voir le détails</a>
-                                        </td>
 
+                                        </div>
                                     </div>
-                                </tr>
-                            @endforeach
+                                </div>
 
 
                         </tbody>
@@ -72,56 +86,6 @@
                 </div>
             </div>
         </div>
-        <!-- <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                @is('admin')
-    <a href="{{ route('contests.projects.create', ['contest' => $contest]) }}">
-                            Nouveau
-                        </a>
-@endis
 
-                <div class="p-6 bg-white border-b border-gray-200">
-                    @foreach ($projects as $project)
-<div>
-                            <a href="{{ route('projects.show', $project->id) }}">
-                                {{ $project->name }}
-                            </a>
-                            @is('admin')
-    <form action="{{ route('projects.destroy', $project->id) }}" method="POST">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="btn btn-danger"
-                                            onclick="return confirm('Êtes-vous sûr de vouloir supprimer cet utilisateur ?')">Supprimer</button>
-                                    </form>
-                                    <div>
-                                        <a href="{{ route('projects.edit', $project->id) }}">Modifier</a>
-                                    </div>
-                                    <div>
-                                        <form action="{{ route('projects.assign', ['project' => $project->id]) }}"
-                                            method="POST">
-                                            @csrf
-                                            <div>
-                                                <label for="manager-choice">Sélectionner un manager :</label>
-                                                <input list="list-manager" id="manager-choice" name="manager-choice"
-                                                    autocomplete="off">
-
-                                                <datalist id="list-manager">
-                                                    @foreach ($managers as $manager)
-    <option value="{{ $manager->user->name }}">
-    @endforeach
-                                                </datalist>
-                                            </div>
-                                            <button type="submit">Assigner</button>
-                                        </form>
-                                    </div>
-@endis
-                        </div>
-@endforeach
-
-
-                </div>
-            </div>
-        </div>
-    </div> -->
+    </div>
 </x-app-layout>
