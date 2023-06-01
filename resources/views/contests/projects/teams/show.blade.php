61766bfeb5e0c0f086562b545b96db9f01a0807d
TiffanyGB <tiffany.gbellile@gmail.com>
2023-06-01 23:04:49 +0200

    final

--- resources/views/contests/projects/teams/show.blade.php
+++ resources/views/contests/projects/teams/show.blade.php
@@ -12,7 +12,24 @@ $user = Auth::user();
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <a class="voirP rounded-lg" href="javascript:history.back()">Revenir au projet</a>
 
-            <div class="bg-white overflow-hidden shadow-lg sm:rounded-lg mt-6 ">
+            <div class="p-6 bg-white border-b border-gray-200 mt-6">
+                    <div class="p-6 bg-white border-gray-200">
+
+                        <p><strong>Membres de l'équipe:</strong></p>
+                        <ul>
+                            @foreach ($team->students as $student)
+                                <li>{{ $student->user->name }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                    @if ($user->student->teams()->where('id', $team->id)->exists())
+                    <x-form action="{{ route('teams.submit', compact('team')) }}" method="PUT">
+                        <x-form-input type="url" name="code" label="Rendu du code" value="{{ $team->code }}" required />
+                        <x-form-submit class="voirP rounded-lg" />
+                    </x-form>
+                @endif
+                </div>
+
                 @is('student')
                 @if (!$user->student->teams()->whereBelongsTo($team->project)->exists())
                     <div class="p-6 bg-white border-b border-gray-200">
@@ -28,34 +45,13 @@ $user = Auth::user();
                         <div class="flex items-center">
                             <form action="{{ route('teams.quit', ['team' => $team]) }}" method="POST">
                                 @csrf
-                                <button type="submit">Quitter l'équipe</button>
+                                <button class="voirP rounded-lg" type="submit">Quitter l'équipe</button>
                             </form>
                         </div>
                     </div>
                 @endif                    
                 @endis
 
-                <div class="p-6 bg-white border-b border-gray-200 ">
-                    <div class="p-6 bg-white border-gray-200">
-
-                        <p><strong>Membres de l'équipe:</strong></p>
-                        <ul>
-                            @foreach ($team->students as $student)
-                                <li>{{ $student->user->name }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
-
-                </div>
-
-                @if ($user->student->teams()->where('id', $team->id)->exists())
-                    <x-form action="{{ route('teams.submit', compact('team')) }}" method="PUT">
-                        <x-form-input type="url" name="code" label="Rendu" value="{{ $team->code }}" required />
-                        <x-form-submit />
-                    </x-form>
-                @endif
-
-            </div>
         </div>
     </div>
 </x-app-layout>
