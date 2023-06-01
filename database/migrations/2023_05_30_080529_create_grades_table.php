33c555f3b465754d1a0a4adc85e710b30c761a65
Lucas Ransan <lucas@ransan.tk>
2023-06-01 19:48:21 +0200

    table grade -> grades

--- database/migrations/2023_05_30_080529_create_grades_table.php
+++ database/migrations/2023_05_30_080529_create_grades_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateGradeTable extends Migration
+class CreateGradesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateGradeTable extends Migration
      */
     public function up()
     {
-        Schema::create('grade', function (Blueprint $table) {
+        Schema::create('grades', function (Blueprint $table) {
             $table->foreignId('quiz_id')->constrained();
             $table->foreignId('team_id')->constrained();
             $table->integer('grade');
@@ -28,6 +28,6 @@ class CreateGradeTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('grade');
+        Schema::dropIfExists('grades');
     }
 }
